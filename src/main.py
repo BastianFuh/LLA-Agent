64ffbd6a43b1ea40ed47e8a9a78876e100f42648
Baeshen <bastian.fuhlenriede@gmail.com>
2025-03-23 20:43:23 +0100

    Remove function which was used to test something

--- src/main.py
+++ src/main.py
@@ -1,13 +1,4 @@
-import gradio as gr
-
 from frontend.gui import create_gui
-
-
-def vote(data: gr.LikeData):
-    if data.liked:
-        print("You upvoted this response: " + data.value["value"])
-    else:
-        print("You downvoted this response: " + data.value["value"])
 
 
 if __name__ == "__main__":
