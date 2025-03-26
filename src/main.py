19d38c97e288eb9bd2b411390e04f71aceeffa8e
Baeshen <bastian.fuhlenriede@gmail.com>
2025-03-26 12:43:55 +0100

    Added local tracing

--- src/main.py
+++ src/main.py
@@ -2,9 +2,15 @@ from frontend.gui import create_gui
 
 import logging
 
+import mlflow
+
 logging.basicConfig(level=logging.INFO)
 
+
 if __name__ == "__main__":
+    mlflow.llama_index.autolog()
+    mlflow.set_experiment("LLA-Agent")
+
     demo = create_gui()
 
     demo.launch()
