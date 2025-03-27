10c4fff06ad158284fc75c704e3417ca0029a12b
Baeshen <bastian.fuhlenriede@gmail.com>
2025-03-27 20:41:25 +0100

    Commented models which do not work

--- src/util/const.py
+++ src/util/const.py
@@ -7,11 +7,15 @@ OPTION_MODEL: list[tuple[str, str]] = [
     ("DeepSeek: V3 0324 (free)", "deepseek/deepseek-chat-v3-0324:free"),
     ("DeepSeek: V3 (free)", "deepseek/deepseek-chat:free"),
     ("DeepSeek: R1 (free)", "deepseek/deepseek-r1:free"),
-    ("Gemini Flash 2.5 Experimental (free)", "google/gemini-2.5-pro-exp-03-25:free"),
-    ("Gemini Flash 2.0 Experimental (free)", "google/gemini-2.0-flash-exp:free"),
-    ("Gemini Pro 2.0 Experimental (free)", "google/gemini-2.0-pro-exp-02-05:free"),
+    ("Mistral: Mistral Nemo (free)", "mistralai/mistral-nemo:free"),
+    ("Meta: Llama 3.1 8B Instruct (free)", "meta-llama/llama-3.1-8b-instruct:free"),
+    # TODO: Find out why these do not work?
+    # ("Gemini Flash 2.5 Experimental (free)", "google/gemini-2.5-pro-exp-03-25:free"),
+    # ("Gemini Flash 2.0 Experimental (free)", "google/gemini-2.0-flash-exp:free"),
+    # ("Gemini Pro 2.0 Experimental (free)", "google/gemini-2.0-pro-exp-02-05:free"),
 ]
 
+NONE = "none"
 GOOGLE = "google"
 TAVILY = "tavily"
-OPTION_SEARCH_ENGINE = [("Google", GOOGLE), ("Tavily", TAVILY)]
+OPTION_SEARCH_ENGINE = [("Google", GOOGLE), ("Tavily", TAVILY), ("None", NONE)]
