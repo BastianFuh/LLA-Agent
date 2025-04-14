59ebfa45b9b82d64b2f13acf24969d3c77fc6efb
Baeshen <bastian.fuhlenriede@gmail.com>
2025-04-15 00:10:27 +0200

    Increased font size

--- src/main.py
+++ src/main.py
@@ -9,12 +9,15 @@ from phoenix.otel import register
 
 import gradio as gr
 
+
 if __name__ == "__main__":
     tracer_provider = register(project_name="LLA-Agent", batch=True, verbose=False)
     LlamaIndexInstrumentor().instrument(tracer_provider=tracer_provider)
 
     logging.basicConfig(level=logging.INFO)
-    with gr.Blocks(fill_height=True) as demo:
+    with gr.Blocks(
+        theme=gr.themes.Default(text_size=gr.themes.sizes.text_lg), fill_height=True
+    ) as demo:
         create_gui()
 
     demo.launch()
